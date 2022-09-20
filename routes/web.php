58a5e5cc8b1c931f78576065e16e1361b19a331a
Xnnms <ilhamgallagher633@gmail.com>
2022-09-20 19:58:51 +0700

    categories navbar add

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::get('blog/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
+        "active" => "categories",
         'categories' => Category::all()
     ]);
 });
@@ -48,6 +49,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('blog', [
         'title' => "Post By Category : $category->name",
+        "active" => "categories",
         'posts' => $category->posts->load('category', 'author'),
     ]);
 });
