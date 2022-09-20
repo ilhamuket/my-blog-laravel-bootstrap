adffb2a7337ecc24e79fa78551c532258f875388
Xnnms <ilhamgallagher633@gmail.com>
2022-09-20 19:43:05 +0700

    update post 3 category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,9 +7,10 @@
                 <h2>
                     {{ $post->title }}
                 </h2>
-                p>By. <a href="/authors/{{ $post->author->username }}"
-                    class="text-decoration-none">{{ $post->author->name }}</a> in
-                <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+                <p>By. <a href="/authors/{{ $post->author->username }}"
+                        class="text-decoration-none">{{ $post->author->name }}</a> in
+                    <a href="/categories/{{ $post->category->slug }}"
+                        class="text-decoration-none">{{ $post->category->name }}</a>
                 </p>
 
                 {!! $post->body !!}
