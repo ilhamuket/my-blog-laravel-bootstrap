af76c0df4c4a6c560e409f87add2207c303dac3d
Xnnms <ilhamgallagher633@gmail.com>
2022-09-21 22:26:02 +0700

    update search slug

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb5">{{ $title }}</h1>
+    <h1 class="mb-3 text-center">{{ $title }}</h1>
+
+    <div class="row mb-3 justify-content-center">
+        <div class="col-md-6">
+            <form action="/blog">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search.." name="search"
+                        value="{{ request('search') }}">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
     @if ($posts->count())
         <div class="card mb-3">
@@ -24,36 +36,37 @@
 
             </div>
         </div>
+
+
+        <div class="container">
+            <div class="row">
+                @foreach ($posts as $post)
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <div class="position-absolute px-3 py-2 " style="background-color: rgba(0, 0, 0, 0.7)">
+                                <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">
+                                    {{ $post->category->name }}</a>
+                            </div>
+                            <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top"
+                                alt="{{ $post->category->name }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $post->title }}</h5>
+                                <p>
+                                    <small class="text-muted">
+                                        By. <a href="/authors/{{ $post->author->username }}"
+                                            class="text-decoration-none">{{ $post->author->name }}</a>
+                                        {{ $post->created_at->diffForhumans() }}
+                                    </small>
+                                </p>
+                                <p class="card-text">{{ $post->excerpt }}</p>
+                                <a href="/blog/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
     @else
         <p class="text-center fs-4">No post found.</p>
     @endif
-
-    <div class="container">
-        <div class="row">
-            @foreach ($posts as $post)
-                <div class="col-md-4 mb-3">
-                    <div class="card">
-                        <div class="position-absolute px-3 py-2 " style="background-color: rgba(0, 0, 0, 0.7)">
-                            <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">
-                                {{ $post->category->name }}</a>
-                        </div>
-                        <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top"
-                            alt="{{ $post->category->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $post->title }}</h5>
-                            <p>
-                                <small class="text-muted">
-                                    By. <a href="/authors/{{ $post->author->username }}"
-                                        class="text-decoration-none">{{ $post->author->name }}</a>
-                                    {{ $post->created_at->diffForhumans() }}
-                                </small>
-                            </p>
-                            <p class="card-text">{{ $post->excerpt }}</p>
-                            <a href="/blog/{{ $post->slug }}" class="btn btn-primary">Read more</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </div>
 @endsection
